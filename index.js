0204de9859a6f0afe9b309c31f4e28e554a5d560
Henil21 <henilsinhrajraj@gmail.com>
2022-08-07 20:57:48 +0530

    7/8

---
+++ index.js
@@ -0,0 +1,11 @@
+var myVar;
+
+function myFunction() {
+ myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+document.querySelector("#loader").style.display = "none";
+document.querySelector("#myDiv").style.display = "block";
+document.querySelector("body").style.backgroundColor = "white";
+}
